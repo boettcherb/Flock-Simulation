ae564daf43d84eaf04b93bfaa6570c1569ce182f
Brandon Boettcher <brandonboettcher19@gmail.com>
2020-04-05 17:59:30 -0500

    Implemented the main loop, boids now move across the canvas

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+
 window.addEventListener('resize', resizeCanvas);
+canvas.addEventListener('click', togglePause);
+
 resizeCanvas();
+let running = true;
 const boids = [];
 for (let i = 0; i < 200; ++i) {
     let X = canvas.width * Math.random();
     let Y = canvas.height * Math.random();
-    boids[boids.length] = new Boid(X, Y);
+    boids.push(new Boid(X, Y));
 }
-drawBoids();
+requestAnimationFrame(mainLoop);
+
+function mainLoop() {
+    clearCanvas();
+    boids.forEach(boid => boid.update());
+    boids.forEach(boid => boid.draw(ctx));
+    if (running) {
+        requestAnimationFrame(mainLoop);
+    }
+}
 
 function resizeCanvas() {
     canvas.width = window.innerWidth * 0.95;
     canvas.height = window.innerHeight * 0.95;
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function togglePause() {
+    running = !running;
+    if (running) {
+        requestAnimationFrame(mainLoop);
+    }
 }
 
 function drawBoids() {
