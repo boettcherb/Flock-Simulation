044b18a8001b40b2be4cc20b301b93a89c55997a
Brandon Boettcher <brandonboettcher19@gmail.com>
2020-04-05 07:04:35 -0500

    Implemented boid rotation and game them variable color

--- boid.js
+++ boid.js
@@ -1,15 +1,36 @@
+const BOID_SIZE = 6.5;
+
 class Boid {
     constructor(x, y) {
         this.x = x;
         this.y = y;
+        this.theta = Math.random() * 2 * Math.PI;
+        this.points = [[], [], []];
+        switch(Math.floor(Math.random() * 5)) {
+            case 0: this.color = '#14e0ff'; break;
+            case 1: this.color = '#34c8f5'; break;
+            case 2: this.color = '#34b4f5'; break;
+            case 3: this.color = '#348cf5'; break;
+            case 4: this.color = '#3478d5'; break;
+        }
     }
 
     draw(ctx) {
-        ctx.fillStyle = '#34b4f5'
+        ctx.fillStyle = this.color;
         ctx.beginPath();
-        ctx.moveTo(this.x, this.y);
-        ctx.lineTo(this.x + 3, this.y + 12);
-        ctx.lineTo(this.x - 3, this.y + 12);
+        this.setPoints(this.theta);
+        ctx.moveTo(this.points[0][0], this.points[0][1]);
+        ctx.lineTo(this.points[1][0], this.points[1][1]);
+        ctx.lineTo(this.points[2][0], this.points[2][1]);
         ctx.fill();
+    }
+
+    setPoints(theta) {
+        this.points[0][0] = BOID_SIZE * Math.sin(theta) + this.x;
+        this.points[0][1] = BOID_SIZE * Math.cos(theta) + this.y;
+        this.points[1][0] = BOID_SIZE * Math.sin(theta + 2.6) + this.x;
+        this.points[1][1] = BOID_SIZE * Math.cos(theta + 2.6) + this.y;
+        this.points[2][0] = BOID_SIZE * Math.sin(theta - 2.6) + this.x;
+        this.points[2][1] = BOID_SIZE * Math.cos(theta - 2.6) + this.y;
     }
 }
--- main.js
+++ main.js
@@ -2,11 +2,19 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 window.addEventListener('resize', resizeCanvas);
 resizeCanvas();
-
-const boid = new Boid(80, 40);
-boid.draw(ctx);
+const boids = [];
+for (let i = 0; i < 200; ++i) {
+    let X = canvas.width * Math.random();
+    let Y = canvas.height * Math.random();
+    boids[boids.length] = new Boid(X, Y);
+}
+drawBoids();
 
 function resizeCanvas() {
     canvas.width = window.innerWidth * 0.95;
     canvas.height = window.innerHeight * 0.95;
+}
+
+function drawBoids() {
+    boids.forEach(boid => boid.draw(ctx));
 }
